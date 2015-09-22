647502c4a2271450d0e8c2fa22acaeaaac42137e
178inaba <178inaba@users.noreply.github.com>
2015-09-23 03:19:05 +0900

    add move pusher

--- Assets/PusherScript.cs
+++ Assets/PusherScript.cs
@@ -3,13 +3,23 @@ using System.Collections;
 
 public class PusherScript : MonoBehaviour {
 
+	Vector3 initPosition;
+	Vector3 newPosition;
+
 	// Use this for initialization
 	void Start () {
-	
+		// save first position
+		initPosition = this.transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		// update position
+		newPosition = new Vector3 (initPosition.x,
+		                           initPosition.y,
+		                           initPosition.z + Mathf.Sin(Time.time));
+
+		// move
+		this.GetComponent<Rigidbody> ().MovePosition (newPosition);
 	}
 }
