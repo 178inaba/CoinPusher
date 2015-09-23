a2b01ecf0397b4a7dc1a2853a0a341108a382a5e
178inaba <178inaba@users.noreply.github.com>
2015-09-23 23:22:06 +0900

    add move spawner

--- Assets/SpawnerScript.cs
+++ Assets/SpawnerScript.cs
@@ -3,13 +3,24 @@ using System.Collections;
 
 public class SpawnerScript : MonoBehaviour {
 
+	float moveSpeed = 2.0f;
+	Rigidbody rb;
+
 	// Use this for initialization
 	void Start () {
-	
+		// get Rigidbody
+		rb = this.GetComponent<Rigidbody> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		// input horizontal key to move
+		float x = Input.GetAxis ("Horizontal");
+
+		// move of x axis
+		Vector3 direction = new Vector3 (x, 0, 0);
+
+		// move speed decision
+		rb.velocity = direction * moveSpeed;
 	}
 }
