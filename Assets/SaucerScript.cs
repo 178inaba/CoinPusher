c4866cf82fed31c5fe9f6ea45115c428031effc4
178inaba <178inaba@users.noreply.github.com>
2015-09-27 23:19:18 +0900

    add SaucerScript

---
+++ Assets/SaucerScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SaucerScript : MonoBehaviour {
+
+	void OnCollisionEnter(Collision colObject) {
+		if (colObject.gameObject.name == "Coin") {
+			Destroy(colObject.gameObject);
+		}
+	}
+}
