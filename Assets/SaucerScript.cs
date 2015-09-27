d9a3ffe6c7b6d29a9564584b82f1f81b99b367ee
178inaba <178inaba@users.noreply.github.com>
2015-09-27 23:44:47 +0900

    fix score

--- Assets/SaucerScript.cs
+++ Assets/SaucerScript.cs
@@ -3,9 +3,19 @@ using System.Collections;
 
 public class SaucerScript : MonoBehaviour {
 
+	public GameObject scoreText;
+	ScoreScript scoreS;
+
+	void Start() {
+		scoreS = scoreText.GetComponent<ScoreScript> ();
+	}
+
 	void OnCollisionEnter(Collision colObject) {
 		if (colObject.gameObject.tag == "Coin") {
+			// delete object
 			Destroy(colObject.gameObject);
+
+			scoreS.addScore(2);
 		}
 	}
 }
